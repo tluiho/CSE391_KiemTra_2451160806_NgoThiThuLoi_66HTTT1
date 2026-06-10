5381fee34b94c80a68dd430d44f6966eacce105e
tluiho <thuloi681307@gmail.com>
2026-06-10 13:53:08 +0700

    Complete HTML layout

---
+++ html/data/data.js
@@ -0,0 +1,7 @@
+const products = [
+    { id: 1, name: "Điện thoại iPhone 15", category: "Điện thoại", price: "24.990.000 ₫", status: true }, // true: Còn hàng
+    { id: 2, name: "Máy tính bảng Samsung Galaxy Tab S9", category: "Máy tính bảng", price: "18.500.000 ₫", status: true },
+    { id: 3, name: "Chuột Logitech MX Master 3S", category: "Phụ kiện", price: "2.490.000 ₫", status: false }, // false: Hết hàng
+    { id: 4, name: "Máy tính xách tay MacBook Air M3", category: "Laptop", price: "31.990.000 ₫", status: true },
+    { id: 5, name: "Tai nghe Sony WH-1000XM5", category: "Tai nghe", price: "7.990.000 ₫", status: true }
+];
